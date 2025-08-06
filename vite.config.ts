4dd2006fb8cba65956c1698c53bb155664299202
lirenkang <lirenkang@lf-network.com>
2025-08-06 16:12:51 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,19 @@ export default defineConfig({
 			'@': path.resolve(__dirname, 'src'),
 		},
     dedupe: ["preact", "preact/hooks"],
-	}
+	},
+  build: {
+    lib: {
+      // 指定你的入口文件
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      // 打包成 ESM 模块
+      formats: ['es'],
+      fileName: 'seel-insurance.js',
+    },
+    rollupOptions: {
+      // 指定哪些依赖要外部引入，而不是打包进 lib 中
+      external: ['preact'],
+    },
+    target: 'es2015',
+  },
 });
